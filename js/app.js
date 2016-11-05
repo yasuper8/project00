135dcd1bab6dd15b8b42bc2b65022b7075c7f783
yasuper8 <ysuper8@gmail.com>
2016-11-04 21:11:16 -0700

    adds img folder and images for players

--- js/app.js
+++ js/app.js
@@ -1,45 +1,3 @@
-// function movePlayer(player){
-//   $(player).css("left", "+=5")
-// }
-//
-//
-// var trackLeftP1 = ( 100 * parseFloat($('.player1').css('left')) / parseFloat($('.container').css('width')) );
-// var trackLeftP2 = ( 100 * parseFloat($('.player2').css('left')) / parseFloat($('.container').css('width')) );
-//
-//
-//
-// function checkWin() {
-//   console.log("checking left val on Player2")
-//   console.log(typeof $(".player2").css("left"))
-//   console.log(( 100 * parseFloat($('.player2').css('left')) / parseFloat($('.container').css('width')) ))
-//   if(( 100 * parseFloat($('.player2').css('left')) / parseFloat($('.container').css('width')) ) > 85){
-//     alert("Player2 Win!!")
-//
-//   }
-//   if(( 100 * parseFloat($('.player1').css('left')) / parseFloat($('.container').css('width')) ) > 85) {
-//     alert("Player1 Win!!")
-//   }
-// }
-//
-// $(document).on("keydown", function(e) {
-//   if (e.which == 76) {
-//     console.log("L is pressed")
-//     $(".player1").css("left", "+=5%")
-//     checkWin();
-//   }
-// })
-// // $("76").keypress(movePlayer("player1"));
-// $(document).on("keydown", function(e) {
-//   if (e.which == 68) {
-//     console.log("D is pressed")
-//     $(".player2").css("left", "+=5%")
-//     console.log($(".player2").css("left"))
-//     console.log($(".container").css("width"))
-//     checkWin();
-//   }
-// })
-//
-
 
 
 var Player = {
@@ -52,6 +10,7 @@ var Player = {
   checkWin: function() {
     if(this.distanceToGoal === 90) {
       alert(this.name + "Wins!");
+      $(window).off("keydown");
     }
   }
 }
@@ -72,23 +31,33 @@ var player2 = PlayerFactory(player2);
 
 $(document).on("keydown", function(e) {
   var $player1 = $(".player1");
-  if (e.which == 76) {
-    console.log("L is pressed");
+  var $player2 = $(".player2");
+  if (e.which === 76) {
+    console.log("l is pressed");
     player1.moveForward();
     $player1.css("left", player1.distanceToGoal + "%");
     console.log(player1.distanceToGoal);
     player1.checkWin();
   }
-});
-
-
-$(document).on("keydown", function(e) {
-  var $player2 = $(".player2");
-  if (e.which == 68) {
-    console.log("D is pressed");
+  if (e.which === 74) {
+    console.log("j is pressed");
+    if(player1.distanceToGoal > 0) {
+    player1.moveBackward();
+    $player1.css("left", player1.distanceToGoal + "%");
+    }
+  }
+  if (e.which === 68) {
+    console.log("d is pressed");
     player2.moveForward();
     $player2.css("left", player2.distanceToGoal + "%");
     console.log(player2.distanceToGoal);
     player2.checkWin();
+  }
+  if (e.which === 65) {
+    console.log("j is pressed");
+    if(player2.distanceToGoal > 0) {
+    player2.moveBackward();
+    $player2.css("left", player2.distanceToGoal + "%");
+    }
   }
 });
