5bfe2f4274d2ca74c9578f354c2bb276af002107
yasuper8 <ysuper8@gmail.com>
2016-11-05 21:48:55 -0700

    adds start count down display and made a 2 raps race to win

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ console.log("main.js linked")
 $( document ).ready(function() {
     console.log( "ready!" );
 
+  var $player1 = $(".player1");
+  var $player2 = $(".player2");
+  var $player2Score = $(".player2Score");
+  var $player1Score = $(".player1Score");
 
   var Player = {
     moveForward: function() {
@@ -11,46 +15,97 @@ $( document ).ready(function() {
       this.distanceToGoal -= 5;
     },
     resetGame: function() {
-      if ((player1.distanceToGoal > 84) || (player2.distanceToGoal > 84)) {
-        player1.distanceToGoal = -4;
-        player2.distanceToGoal = -4;
+      if ((player1.distanceToGoal >= 90) || (player2.distanceToGoal >= 90)) {
+        player1.distanceToGoal = 1;
+        player2.distanceToGoal = 1;
+        $player1.css("left", player1.distanceToGoal + "%");
+        $player2.css("left", player2.distanceToGoal + "%");
       }
     },
     checkWin: function() {
-      if(this.distanceToGoal === 85) {
-        // player1.distanceToGoal = 0;
-        // player2.distanceToGoal = 0;
-        // game.resetGame();
+      if((this.numTracks === 1) && (this.distanceToGoal >= 90)) {
         this.score += 1;
         console.log(this.score);
+        this.countTracks();
         this.resetGame();
+        this.displayScore();
         alert(this.name + " Wins!");
-        this.resetGame();
-        // $(window).off("keydown");
-
       }
+    },
+    displayScore: function() {
+      if (player1.score === 1) {
+        $player1Score.text("Player 1: " + player1.score + " Win");
+      }
+      if (player1.score > 1) {
+        $player1Score.text("Player 1: " + player1.score + " Wins");
+      }
+      if (player2.score === 1) {
+        $player2Score.text("Player 1: " + player2.score + " Win");
+      }
+      if (player2.score > 1) {
+        $player2Score.text("Player 1: " + player2.score + " Wins");
+      }
+    },
+    countTracks: function() {
+        this.numTracks += 1;
     }
   }
 
 
-  function resteGame() {
-
-      player1.distanceToGoal = 0;
-      player2.distanceToGoal = 0;
-
-  }
-
-  // var Game = {
-  //   resetGame: function() {
+  // function resteGame() {
+  //
   //     player1.distanceToGoal = 0;
   //     player2.distanceToGoal = 0;
-  //   }
+  //
   // }
 
+  var Game = {
+    addNumGame: function() {
+      this.numGame += 1;
+    },
+    countStart: function() {
+      $(".counter").text("Ready!");
+      setTimeout(function() {
+        $(".counter").text("Three");
+      }, 1000);
+      setTimeout(function() {
+        $(".counter").text("Two");
+      }, 2000);
+      setTimeout(function() {
+        $(".counter").text("One");
+      }, 3000);
+      setTimeout(function() {
+        $(".counter").text("Go!!!!");
+        $(".counter").css("font-size", "3em");
+      }, 4000);
+    }
+  }
+
+
+
+$(".startBtn").on('click', function() {
+  console.log("start clicked");
+  for(var i = 3; i >= 0; i--) {
+    console.log(i)
+    game.countStart();
+  }
+});
+  //
+  // $("form").on('submit', function(event) {
+  //       event.preventDefault();
+  //       console.log("Button is clicked")
+  //       var $username = $("input.userName").val();
+  //
+  //       var player1 = PlayerFactory($username);
+  //
+  //     });
+  //   });
+  //
 
   function GameFactory(){
     var game = Object.create(Game);
     game.numGame = 0;
+    game.startCounter = 3;
     return game;
   }
 
@@ -60,6 +115,7 @@ $( document ).ready(function() {
     player.score = 0;
     player.time = 0;
     player.distanceToGoal = 0;
+    player.numTracks = 0;
     return player;
   }
   //
@@ -115,17 +171,16 @@ $( document ).ready(function() {
 
 
   $(document).on("keydown", function(e) {
-    var $player1 = $(".player1");
-    var $player2 = $(".player2");
+
     if (e.which === 76) {
       console.log("l is pressed");
       if (player1.distanceToGoal < 90) {
         player1.moveForward();
         $player1.css("left", player1.distanceToGoal + "%");
-        console.log(player1.distanceToGoal);
         player1.checkWin();
-        if (player1.distanceToGoal > 84) {
-        // player1.resetGame();
+        if ((player1.distanceToGoal === 90) && (player1.numTracks === 0)) {
+          player1.distanceToGoal = 0;
+          player1.countTracks();
         }
       }
     }
@@ -143,6 +198,10 @@ $( document ).ready(function() {
         $player2.css("left", player2.distanceToGoal + "%");
         console.log(player2.distanceToGoal);
         player2.checkWin();
+        if ((player2.distanceToGoal === 90) && (player2.numtracks === 0)) {
+          player2.distanceToGoal = 0;
+          player2.countTracks();
+        }
       }
     }
     if (e.which === 65) {
@@ -154,9 +213,11 @@ $( document ).ready(function() {
     }
   });
 
+
+// $player1.animate({"left": player1.distanceToGoal + "%"}, "fast");
   // var shark = SharkFactory();
   var player1 = PlayerFactory("player1");
   var player2 = PlayerFactory("player2");
-  // var game = GameFactory();
+  var game = GameFactory();
 
 }); //end ready
--- js/main2.js
+++ js/main2.js
@@ -2,10 +2,90 @@ console.log("main2.js linked!")
 $( document ).ready(function() {
     console.log( "ready!" );
 
-var $player1 = $(".player1");
-var $player2 = $(".player2");
-var distanceToGoalP1 = 0;
-var distanceToGoalP2 = 0;
+  var $player1 = $(".player1");
+  var $player2 = $(".player2");
+  var distanceToGoalP1 = 0;
+  var distanceToGoalP2 = 0;
+
+  function moveForward(player) {
+    if (player === $player1) {
+      distanceToGoalP1 += 5;
+    }
+    if (player === $player2) {
+      distanceToGoalP2 += 5;
+    }
+  }
+
+  function moveBackward(player) {
+    if (player === $player1) {
+      distanceToGoalP1 -= 5;
+    }
+    if (player === $player2){
+      distanceToGoalP2 -= 5;
+    }
+  }
+
+  function checkWin() {
+    if (distanceToGoalP1 === 85) {
+      resetGame();
+      alert("Player1 Win!");
+
+    }
+    if (distanceToGoalP2 === 85) {
+      resetGame();
+      alert("Player2 Win!");
+    }
+  }
+
+  function resetGame() {
+    distanceToGoalP1 = -4;
+    distanceToGoalP2 = -4;
+    $player1.css("left", distanceToGoalP1 + "%");
+    $player2.css("left", distanceToGoalP2 + "%");
+  }
+
+  $(document).on("keydown", function(e) {
+
+    if (e.which === 76) {
+      console.log("l is pressed");
+      if (distanceToGoalP1 < 90) {
+        moveForward($player1);
+        $player1.css("left", distanceToGoalP1 + "%");
+        console.log(distanceToGoalP1);
+        checkWin()
+        if (distanceToGoalP1 > 84) {
+        // player1.resetGame();
+        }
+      }
+    }
+    if (e.which === 74) {
+      console.log("j is pressed");
+      if(distanceToGoalP1 > 0) {
+      moveBackward($player1);
+      $player1.css("left", distanceToGoalP1 + "%");
+      }
+    }
+    if (e.which === 68) {
+      console.log("d is pressed");
+      if (distanceToGoalP2 < 90) {
+        moveForward($player2);
+        $player2.css("left", distanceToGoalP2 + "%");
+        console.log(distanceToGoalP2);
+        checkWin();
+      }
+    }
+    if (e.which === 65) {
+      console.log("j is pressed");
+      if(distanceToGoalP2 > 0) {
+      moveBackward($player2);
+      $player2.css("left", distanceToGoalP2 + "%");
+      }
+    }
+    if (e.which === 71 ) {
+      console.log("g pressed");
+      resetGame();
+    }
+  });
 
 
 
